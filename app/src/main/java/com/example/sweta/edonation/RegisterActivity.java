0cbf42d4c125de0a0498175fa7820bf50abced3d
Your Name <sw3taprajapati>
2018-05-15 21:55:26 +0545

    registerActivity done

---
+++ app/src/main/java/com/example/sweta/edonation/RegisterActivity.java
@@ -0,0 +1,46 @@
+package com.example.sweta.edonation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        initComponent();
+        initToolbar();
+    }
+
+    private void initComponent(){
+        toolbar=findViewById(R.id.toolBar);
+    }
+
+    private void initToolbar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Register Account");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
+    }
+
+}
